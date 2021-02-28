316d6f38fec6511ee2868e0a88f5bcc4c29a5700
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-02-28 13:41:59 -0300

    inclusao dos fontes do projeto de teste de listagem de contatos usando ionic capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from "@capacitor/core";
+const  { Contacts } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+ contacts:any[] = [];
 
   constructor() {}
 
+  ngOnInit(){
+    Contacts.getContacts().then(result => {
+      console.log(result);
+
+      for (const contact of result.contacts) {
+        if(contact.phoneNumbers[0] != undefined){
+           this.contacts.push(contact);
+        }
+
+//        alert(contact.displayName + '\n' + contact.phoneNumbers);
+     }
+
+      for (const contact of result.contacts) {
+          alert(JSON.stringify(contact));
+      }
+  });
+  }
 }
